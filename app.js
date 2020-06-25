22c2a230e88a283abcc72e62227d3cad999e8482
Sacha Sedriks <sachased@gmail.com>
2020-06-24 17:52:50 -0700

    updated with some basic routing, e.g. app.get('/path', handler).

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ app.get('/', (req, res) => {
   res.send('Hello Express!');
 });
 
+app.get('/foo', (req, res) => {
+  res.send('We have foo.')
+});
+app.get('/foo/bar', (req, res) => {
+  res.send('We have bar within the foo.')
+});
+app.get('/foo/bizz', (req, res) => {
+  res.send('We have bizz within the foo, too.')
+});
+
+
 app.listen(8000, () => {
   console.log('Express server is listening on port 8000...')
 });
